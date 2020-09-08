12afea2031988e25df0758a00e33bc6deb2bf59f
Simone Bacchio <s.bacchio@gmail.com>
2020-09-08 17:31:21 +0300

    Adding tests for parallel

--- test/test_solver.py
+++ test/test_solver.py
@@ -1,5 +1,7 @@
+from lyncs_mpi import Client
 from lyncs_DDalphaAMG import Solver
 import numpy as np
+from distributed import wait
 
 
 def test_init():
@@ -18,3 +20,37 @@ def test_init():
     vec[0, 0, 0, 0, 0, 0] = 1
     sol = solver.solve(vec)
     assert np.allclose(solver.D(sol), vec)
+
+
+def test_mpi():
+    client = Client(2)
+    comms = client.create_comm()
+
+    procs = [2, 1, 1, 1]
+    comms = comms.create_cart(procs)
+    create_solver = lambda comm: Solver(
+        global_lattice=[4, 4, 4, 4],
+        block_lattice=[2, 2, 2, 2],
+        procs=[1, 1, 1, 1],
+        kappa=0.1,
+        comm=comm,
+    )
+    solver = client.map(create_solver, comms)
+    assert len(solver) == 2
+    wait(solver)
+
+
+def test_parallel():
+    client = Client(2)
+    comms = client.create_comm()
+
+    procs = [2, 1, 1, 1]
+    comms = comms.create_cart(procs)
+    solver = Solver(
+        global_lattice=[4, 4, 4, 4],
+        block_lattice=[2, 2, 2, 2],
+        procs=[1, 1, 1, 1],
+        kappa=0.1,
+        comm=comms,
+    )
+    assert len(solver) == 2
